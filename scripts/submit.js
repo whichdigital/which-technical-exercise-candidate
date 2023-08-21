7d03a1e0f25525334218d7698aeb6e6f12fd1e24
Matthew Denner <matthew.denner@which.co.uk>
2023-08-21 08:44:53 +0100

    Guarding against a few issues that have appeared with uploads

--- scripts/submit.js
+++ scripts/submit.js
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
 
-const https = require('https');
-const fs = require('fs');
-
+// Guard against a couple of common issues with how this script gets executed.
 const candidateSubmission = process.argv[2];
-const submissionContents = fs.readFileSync(candidateSubmission);
+if (candidateSubmission === 'submission-true.zip') {
+  console.log('Please ensure you use `--candidate=your-email-address`. Note the equals sign in that.');
+  process.exit(1);
+} else if (!candidateSubmission.match(/^submission-([^@]+@.+)\.zip$/)) {
+  console.log(
+    'This script is intended to be run as part of the submission process. Please refer to the README.md file for instructions'
+  );
+  process.exit(1);
+}
 
 console.log('Uploading your submission to Which? systems ...');
 
+const fs = require('fs');
+const submissionContents = fs.readFileSync(candidateSubmission);
+
+const https = require('https');
 const request = https.request(
   {
     host: 'api.interviews.internal.which.co.uk',
@@ -34,14 +44,15 @@ const request = https.request(
     });
   }
 );
-request.write(submissionContents);
-request.end();
-
+request.shouldKeepAlive = false;
 request.on('error', (error) => {
   console.error(
     'There was a problem uploading your submission. Try again. If it continues please contact Which? via email.',
     error.message
   );
 });
+
+request.write(submissionContents);
+request.end();
 
 console.log("Submission upload complete. Please let Which? know you've done this just in case!");
