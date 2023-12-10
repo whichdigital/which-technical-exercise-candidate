a816b82e990666f8aafe3eaf076e7593f1c9fd92
David Benson <dmbenson1978@gmail.com>
2023-12-10 18:59:51 +0000

    Alternative implementation of `setHeading()`
    Signed-off-by: David Benson <dmbenson1978@gmail.com>

--- src/lib.ts
+++ src/lib.ts
@@ -1,6 +1,18 @@
 import { Headings, Turns } from '.';
 
 export const setHeading = (currentHeading: Headings, direction: Turns): Headings => {
+  const allHeadings = ['north', 'east', 'south', 'west'] as Headings[];
+  let newIndex = -1;
+
+  switch (direction) {
+    case 'left':
+      newIndex = allHeadings.indexOf(currentHeading) - 1;
+      return newIndex < 0 ? allHeadings[allHeadings.length - 1] : allHeadings[newIndex];
+    case 'right':
+      newIndex = allHeadings.indexOf(currentHeading) + 1;
+      return newIndex > allHeadings.length - 1 ? allHeadings[0] : allHeadings[newIndex];
+  }
+
   switch (currentHeading) {
     case 'east':
       return direction === 'left' ? 'north' : 'south';
