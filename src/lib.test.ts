db3241c5819cd9c967ec339f0abe673ca4a6418e
David Benson <dmbenson1978@gmail.com>
2023-12-10 18:29:23 +0000

    Update `setHeading()` test
    6/8 tests fail - expected result as `setHeading()` only returns `west`
    
    Signed-off-by: David Benson <dmbenson1978@gmail.com>

--- src/lib.test.ts
+++ src/lib.test.ts
@@ -1,10 +1,40 @@
 import { describe, expect, test } from '@jest/globals';
 import { setHeading } from './lib';
+import { Headings, Turns } from '.';
+
+const testSetHeading = (heading: Headings, turn: Turns) => {
+  const results = {
+    north: {
+      left: 'west',
+      right: 'east',
+    },
+    east: {
+      left: 'north',
+      right: 'south',
+    },
+    south: {
+      left: 'east',
+      right: 'west',
+    },
+    west: {
+      left: 'south',
+      right: 'north',
+    },
+  };
+  test(`robot turns ${turn}`, () => {
+    expect(setHeading(heading, turn)).toEqual(results[heading][turn]);
+  });
+};
 
 describe('Robot heading:', () => {
-  describe('When facing north', () => {
-    test('robot turns left', () => {
-      expect(setHeading('north', 'left')).toEqual('west');
+  const turns: Turns[] = ['left', 'right'];
+  const headings: Headings[] = ['north', 'south', 'east', 'west'];
+
+  headings.forEach((heading) => {
+    describe(`When facing ${heading}`, () => {
+      turns.forEach((turn) => {
+        testSetHeading(heading, turn);
+      });
     });
   });
 });
